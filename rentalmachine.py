980712be6656fa23b4b5d7e5d328463c51d9982c
Mugeruka-1 <nadiamugeruka@gmail.com>
2023-08-05 18:36:15 +0200

    Declaration of variables to be used

---
+++ rentalmachine.py
@@ -0,0 +1,8 @@
+print("--------WELCOME TO OUR AGRICULTURAL RENTING MACHINE----------")
+print("We have the following machines. Please choose any machine you want:")
+print("1. Plow per one hour 1000/h")
+print("2. Disc Harrow 1500/h")
+print("3. Rotary Tiller 2000/h")
+print("4. Cultivator 800/h")
+print("5. Subsoiler 1200/h")
+
