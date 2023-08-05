f03ed70ec8d264a7632d9853cf4fa413136b0fc3
Liliane Kayitesi <l.kayitesi@alustudent.com>
2023-08-05 19:49:20 +0200

    line 11

--- rentalmachine.py
+++ rentalmachine.py
@@ -8,3 +8,4 @@ print("5. Subsoiler 1200/h")
 
 machine = int(input("Please choose any type of the machine(enter a number betueen 1 and 5):"))
 if starting_hour >= ending_hour:
+print("The starting hour must be less than the ending hour")
